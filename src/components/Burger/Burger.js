edf16ca74155ba3a663973abcc133a8355351e25
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-02 11:05:22 -0600

    Used the reduce method to flatten the array and then check if number of ingredients is = 0

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,18 @@ import BurgerIngredient from './BurgerIndgredient/BurgreIngredient';
 
 const burger = (props) => {
 	//Converts an object to an array for use in determining the number of ingredients
-	const transformedIngredients = Object.keys(props.ingredients)
+	let transformedIngredients = Object.keys(props.ingredients)
 		.map(igKey => {
 			return [...Array(props.ingredients[igKey])].map((_, i) => {
 				return <BurgerIngredient key={igKey + i} type={igKey} />
 			});
-		});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el)
+		}, []);
+	if(transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients!</p>
+	}
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type='bread-top' />
