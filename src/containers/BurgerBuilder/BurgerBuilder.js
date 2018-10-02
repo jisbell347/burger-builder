cc6704851991d97901f1f538015eef5e883e3b86
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-02 12:14:55 -0600

    Added ingredientRemoved property to BuildControls component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -53,6 +53,7 @@ class BurgerBuilder extends Component {
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
 					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
 				/>
 			</Auxiliary>
 		);
