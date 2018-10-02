b3a1a3df307279613266c00127fd7b53e9e6a7a9
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-02 11:22:03 -0600

    Added initial BuildControl component and jsx

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const buidControl = (props) => (
+	<div>
+		<div>{prop.label}</div>
+		<button>Less</button>
+		<button>More</button>
+	</div>
+);
+
+export default buidControl;
