1afe18c447f30251f42ad37ddea1a41b2e805f06
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-02 10:57:37 -0600

    Converted the state object into an array and returned the burgerIngredients based on the number of ingredients inside the state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIndgredient/BurgreIngredient';
 
 const burger = (props) => {
+	//Converts an object to an array for use in determining the number of ingredients
+	const transformedIngredients = Object.keys(props.ingredients)
+		.map(igKey => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />
+			});
+		});
 	return (
 		<div className={classes.Burger}>
 			<BurgerIngredient type='bread-top' />
-			<BurgerIngredient type='cheese' />
-			<BurgerIngredient type='meat' />
+			{transformedIngredients}
 			<BurgerIngredient type='bread-bottom' />
 		</div>
 	);
