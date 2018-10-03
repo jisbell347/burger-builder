d4cd9049ac0224dd453c6737fb76b1e0553b440e
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-03 12:50:54 -0600

    Created a purchaseContinueHandler method

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,7 +74,11 @@ class BurgerBuilder extends Component {
 
 	purchaseCancelHandler = () => {
 		this.setState({purchasing: false});
-	}
+	};
+
+	purchaseContinueHandler = () => {
+		alert('You continue!')
+	};
 
 	render() {
 		const disabledInfo = {
@@ -86,7 +90,10 @@ class BurgerBuilder extends Component {
 		return(
 			<Auxiliary>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients}/>
+					<OrderSummary
+						purchaseCancelled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler}
+						ingredients={this.state.ingredients}/>
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
 				<BuildControls
