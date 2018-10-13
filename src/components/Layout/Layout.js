45afc1cd1d00ee4541eac0b75093067ae2599026
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-13 09:42:47 -0600

    Moved Auxiliary.js into Auxiliary directory

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
